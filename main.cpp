a6f43e3448742a78e6ede3daae6ee2642cb800fc
havenotdrugs <132453806+havenotdrugs@users.noreply.github.com>
2023-05-03 14:19:01 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//Задание 1.4
+int main() {
+  float a, b;
+  cout << "Привет. Я делю заданные тобой числа.\nВведи первое число: ";
+  cin >> a;
+  cout << "Введи второе число: ";
+  cin >> b;
+  cout << "Частное твоих чисел - " << a / b;
+  return 0;
+}
